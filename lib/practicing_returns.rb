df3987fd7a6ca0efdd89e4abd0ffc6933646e085
Pamela Frieze <pfrieze@gmail.com>
2017-10-08 00:43:19 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -10,5 +10,6 @@ end
 
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
 #Arrays are great for storing lists of information
 #Block is code inside do and end or in-line between { and }
